129ee8458e4ea5cc583c2e2a0e52216fc7020cbc
Clement WANG <clementwang2001@gmail.com>
2023-08-29 14:17:49 +0200

    feat: cleaned up code

--- src/data_cleaning/controller.py
+++ src/data_cleaning/controller.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
 
-from src.data_cleaning.utils import to_binary_one_hot, to_one_hot
+from sklearn.model_selection import train_test_split
+
+from src.data_cleaning.utils import to_binary_one_hot, to_one_hot, unique_values
 
 
 def import_opportunities() -> pd.DataFrame:
@@ -10,47 +13,19 @@ def import_opportunities() -> pd.DataFrame:
     return df
 
 
-def load_opportunities() -> pd.DataFrame:
-    df = import_opportunities()
+def drop_double_opportunities(df: pd.DataFrame) -> pd.DataFrame:
+    return df.drop_duplicates(subset=["N° Opportunité"])
 
-    keep_cols = [
-        "Montant",
-        "Origine de l'opportunité",
-        "Opération",
-        "Domaine",
-        "Position",
-        "Type du compte",
-        "Étape",
-        "Date de dernière modification",
-    ]
 
-    binary_one_hot_cols = ["Opération", "Domaine", "Position"]
-
-    one_hot_cols = [
-        "Origine de l'opportunité",
-        "Type du compte",
-    ]
-
-    df = df[keep_cols]
-
-    # 'Montant' to float
-
+def process_montant(df: pd.DataFrame) -> pd.DataFrame:
     df["Montant"] = df["Montant"].str.replace(",", ".").astype(float)
-
-    # To one hot
-
-    df = to_binary_one_hot(df, binary_one_hot_cols)
-
-    df = to_one_hot(df, one_hot_cols)
-
-    # Drop NaN in 'Montant'
-
     df = df.dropna(subset=["Montant"])
-
-    # Drop negative values in 'Montant'
-
     df = df[df["Montant"] > 0]
 
+    return df
+
+
+def create_labels(df: pd.DataFrame) -> pd.DataFrame:
     df["Label"] = None
 
     # if 'Étape' in ['6- Gagnée', '9- Gagnée archivée'] then 'Label' = 1
@@ -86,3 +61,326 @@ def load_opportunities() -> pd.DataFrame:
     )
 
     return df
+
+
+def load_staff(opportunities: pd.DataFrame) -> pd.DataFrame:
+    staff = pd.read_csv("data/Staff.csv", sep=";", encoding="ISO-8859-1")
+
+    staff = staff[staff["Identifiant"].isin(opportunities["Resp..1"])]
+
+    keep_cols_staff = [
+        "Identifiant",
+        "fDateDeNaissance",
+        "fDateEntree",
+        "sDiplomeFinal",
+        "sFicheDePoste",
+    ]
+
+    staff = staff[keep_cols_staff]
+
+    one_hot_cols_staff = ["sDiplomeFinal", "sFicheDePoste"]
+
+    staff = to_one_hot(staff, one_hot_cols_staff)
+
+    staff["age"] = (
+        pd.to_datetime("today")
+        - pd.to_datetime(staff["fDateDeNaissance"], format="%d/%m/%Y")
+    ).dt.days / 365
+
+    staff["anciennete"] = (
+        pd.to_datetime("today")
+        - pd.to_datetime(staff["fDateEntree"], format="%d/%m/%Y")
+    ).dt.days / 365
+
+    staff = staff.drop(["fDateDeNaissance", "fDateEntree"], axis=1)
+
+    return staff
+
+
+def join_staff(df: pd.DataFrame) -> pd.DataFrame:
+    # Clean 'Resp.' and 'Resp..1'
+    df.loc[
+        (df["Resp..1"] == "LIL") & (df["Resp."] == "Julien MILLE"), "Resp..1"
+    ] = "MIL"
+
+    staff = load_staff(df)
+
+    df = df.join(staff.set_index("Identifiant"), on="Resp..1")
+
+    df = df.drop(["Resp..1", "Resp."], axis=1)
+
+    return df
+
+
+def separate_code_postal(insee_com: pd.DataFrame) -> pd.DataFrame:
+    """'Code Postal' can be a list of code postal separated by '/'"""
+    insee_com_single = insee_com[insee_com["Code Postal"].map(lambda x: len(x) == 5)]
+
+    insee_com_multiple = []
+
+    for _, line in insee_com[
+        insee_com["Code Postal"].map(lambda x: len(x) != 5)
+    ].iterrows():
+        for code_postal in line["Code Postal"].split("/"):
+            item = line.copy()
+            item["Code Postal"] = code_postal
+            insee_com_multiple.append(item)
+
+    insee_com_multiple = pd.DataFrame(insee_com_multiple)
+
+    return pd.concat((insee_com_multiple, insee_com_single))
+
+
+def correct_missing_postals(df: pd.DataFrame, insee_com: pd.DataFrame) -> pd.DataFrame:
+    missing_postals = df.loc[
+        ~df["code_postal"].isin(insee_com["Code Postal"]), "code_postal"
+    ].unique()
+    missing_postals = [str(x) for x in missing_postals if x != np.nan]
+
+    insee_com_missing_postals = []
+
+    insee_com["hash_commune"] = insee_com["Commune"].str.lower()
+
+    manual_corrected_postals = {
+        "lyon": "69001",
+        "marseille": "13001",
+        "paris": "75001",
+        "strasbourg cedex": "67000",
+        "sarreguemines": "57200",
+    }
+
+    for _, line in (
+        df[df["code_postal"].isin(missing_postals)]
+        .drop_duplicates(subset=["nom_ville", "code_postal"])
+        .iterrows()
+    ):
+        if line["nom_ville"] in insee_com["hash_commune"].unique():
+            sub_df = insee_com[insee_com["hash_commune"] == line["nom_ville"]]
+            insee_line = sub_df.loc[sub_df["Population"].idxmax()]
+            if len(insee_line.shape) > 1:
+                insee_line = insee_line.iloc[0]
+            insee_line["Code Postal"] = line["code_postal"]
+
+            insee_com_missing_postals.append(insee_line)
+
+        elif line["nom_ville"] in manual_corrected_postals:
+            sub_df = insee_com[
+                insee_com["Code Postal"] == manual_corrected_postals[line["nom_ville"]]
+            ]
+            insee_line = sub_df.loc[sub_df["Population"].idxmax()]
+            if len(insee_line.shape) > 1:
+                insee_line = insee_line.iloc[0]
+            insee_line["Code Postal"] = line["code_postal"]
+
+            insee_com_missing_postals.append(insee_line)
+
+        else:
+            # print(line["nom_ville"], line["code_postal"])
+            ()
+
+    insee_com_missing_postals = pd.DataFrame(insee_com_missing_postals)
+
+    return pd.concat([insee_com, insee_com_missing_postals])
+
+
+def join_insee_communes(df: pd.DataFrame) -> pd.DataFrame:
+    # Correct miss filled 'Ville'
+    df.loc[df["Ville"] == "*75019*", "Ville"] = "75019 PARIS-19E__ARRONDISSEMENT"
+
+    # Separate 'Ville' in 'nom_ville' and 'code_postal' when possible
+    df["nom_ville"] = df["Ville"].apply(
+        lambda x: x[6:].lower() if str(x)[:5].isdigit() else np.nan
+    )
+    df["code_postal"] = df["Ville"].apply(
+        lambda x: x[:5] if str(x)[:5].isdigit() else np.nan
+    )
+    df = df.copy()
+
+    # Import 'correspondance-code-insee-code-postal.csv'
+    insee_com = pd.read_csv("data/correspondance-code-insee-code-postal.csv", sep=";")
+
+    insee_com = separate_code_postal(insee_com)
+
+    insee_com = correct_missing_postals(df, insee_com)
+
+    insee_com["densite"] = insee_com["Population"] / insee_com["Superficie"]
+    insee_com["longitude"] = insee_com["geo_point_2d"].apply(
+        lambda x: float(x.split(",")[0]) if type(x) == str else np.nan
+    )
+    insee_com["latitude"] = insee_com["geo_point_2d"].apply(
+        lambda x: float(x.split(",")[1]) if type(x) == str else np.nan
+    )
+
+    df = pd.merge(
+        df, insee_com, left_on="code_postal", right_on="Code Postal", how="left"
+    )
+
+    df = df.drop(
+        [
+            "Ville",
+            "nom_ville",
+            "code_postal",
+            "Code Postal",
+            "Commune",
+            "Département",
+            "Région",
+            "Statut",
+            "geo_point_2d",
+            "geo_shape",
+            "ID Geofla",
+            "Code Commune",
+            "Code Canton",
+            "Code Arrondissement",
+            "Code Département",
+            "Code Région",
+            "hash_commune",
+        ],
+        axis=1,
+    )
+
+    return df
+
+
+def join_prixm2(df: pd.DataFrame) -> pd.DataFrame:
+    prixm2 = pd.read_csv("data/prixm2-communes-2017.csv", sep=",")
+    prixm2 = prixm2[["INSEE_COM", "Prixm2"]]
+
+    df = pd.merge(df, prixm2, left_on="Code INSEE", right_on="INSEE_COM", how="left")
+
+    df = df.drop(["INSEE_COM"], axis=1)
+
+    return df
+
+
+def load_opportunities() -> pd.DataFrame:
+    df = import_opportunities()
+
+    # Drop double in 'N° Opportunité'
+    df = drop_double_opportunities(df)
+
+    # Filter columns
+    keep_cols = [
+        "Montant",
+        "Origine de l'opportunité",
+        "Opération",
+        "Domaine",
+        "Position",
+        "Type du compte",
+        "Étape",
+        "Date de dernière modification",
+        "Activité",
+        "Rôle du contact",
+        "Resp.",
+        "Resp..1",
+        "Ville",
+    ]
+    df = df[keep_cols]
+
+    # 'Montant' to float, drop NaN in 'Montant', drop negative values in 'Montant'
+    df = process_montant(df)
+
+    # One hot encoding
+    binary_one_hot_cols = ["Opération", "Domaine", "Position"]
+    df = to_binary_one_hot(df, binary_one_hot_cols)
+
+    one_hot_cols = [
+        "Origine de l'opportunité",
+        "Type du compte",
+        "Activité",
+        "Rôle du contact",
+    ]
+    df = to_one_hot(df, one_hot_cols)
+
+    # Create 'Label'
+    df = create_labels(df)
+
+    df = join_staff(df)
+
+    df = join_insee_communes(df)
+
+    df = join_prixm2(df)
+
+    df = df.drop(["Code INSEE"], axis=1)
+
+    return df
+
+
+def load_train_test_split(
+    drop_na: bool = False, test_size: float = 0.2, random_state: int = 42
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    opportunities = load_opportunities()
+    if drop_na:
+        opportunities = opportunities.dropna()
+
+    labels = opportunities["Label"].astype(bool)
+    features = opportunities.drop("Label", axis=1)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        features,
+        labels,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=labels,
+    )
+
+    return X_train, y_train, X_test, y_test
+
+
+def load_opportunities_for_chantiers() -> pd.DataFrame:
+    df = import_opportunities()
+
+    # Drop double in 'N° Opportunité'
+    df = drop_double_opportunities(df)
+
+    keep_cols = [
+        "N° Opportunité",
+        "Montant",
+        "Origine de l'opportunité",
+        "Opération",
+        "Domaine",
+        "Position",
+        "Type du compte",
+        "Étape",
+        "Date de dernière modification",
+        "Activité",
+        "Rôle du contact",
+        "Resp.",
+        "Resp..1",
+        "Ville",
+    ]
+
+    df = df[keep_cols]
+
+    # 'Montant' to float, drop NaN in 'Montant', drop negative values in 'Montant'
+    df = process_montant(df)
+
+    # One hot encoding
+
+    binary_one_hot_cols = ["Opération", "Domaine", "Position"]
+    df = to_binary_one_hot(df, binary_one_hot_cols)
+
+    one_hot_cols = [
+        "Origine de l'opportunité",
+        "Type du compte",
+        "Activité",
+        "Rôle du contact",
+    ]
+    df = to_one_hot(df, one_hot_cols)
+
+    # Create 'Label'
+
+    df = create_labels(df)
+
+    # Clean 'Resp.' and 'Resp..1'
+
+    df.loc[
+        (df["Resp..1"] == "LIL") & (df["Resp."] == "Julien MILLE"), "Resp..1"
+    ] = "MIL"
+
+    staff = load_staff(df)
+
+    joint = df.join(staff.set_index("Identifiant"), on="Resp..1")
+
+    joint = joint.drop(["Resp..1", "Resp."], axis=1)
+
+    return joint
